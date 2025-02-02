1670a939c7e22d1c5daadbb857f091add3fca23b
morshedulmunna <morshedulmunna1@gmail.com>
2025-02-02 16:12:37 +0600

    cc

--- product-service/build.rs
+++ product-service/build.rs
@@ -1,6 +1,18 @@
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    tonic_build::compile_protos("protobuf/order_product.proto")?;
+    // List of .proto files
+    let proto_files = &[
+        "../protobuf/order_product.proto",
+        "../protobuf/product.proto",
+    ];
+
+    // Specify the include path for .proto imports
+    let proto_include = "../protobuf";
+
+    tonic_build::configure()
+        .build_client(true)
+        .build_server(true)
+        .compile_protos(proto_files, &[proto_include])?;
     Ok(())
 }
